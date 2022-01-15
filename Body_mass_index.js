a3b1445bb5792a07e51ca285702bdf10b676afa3
rasel <russelroy15@gmail.com>
2022-01-16 02:03:12 +0600

    BMI calculation

--- Body_mass_index.js
+++ Body_mass_index.js
@@ -4,3 +4,8 @@ const massAsif = 77;
 const hightAsif = 1.67;
 const massShakib = 80;
 const hightShakib = 1.7;
+
+const BMIAsif = massAsif / hightAsif ** 2;
+const BMIShakib = massShakib / (hightShakib * hightShakib);
+
+console.log(BMIAsif, BMIShakib);
