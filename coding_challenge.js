cabe248e85032d809d4cd46db2a0ddc8c5bc9e48
rasel <russelroy15@gmail.com>
2022-01-16 17:32:54 +0600

    coding challenge code

--- coding_challenge.js
+++ coding_challenge.js
@@ -1,9 +1,14 @@
 //coding challenge
 
-const massMark = 95;
-const hightMark = 1.88;
-const massJohn = 85;
-const hightJohn = 1.76;
+// const massMark = 95;
+// const hightMark = 1.88;
+// const massJohn = 85;
+// const hightJohn = 1.76;
+
+const massMark = 78;
+const hightMark = 1.69;
+const massJohn = 92;
+const hightJohn = 1.95;
 
 const BMIMark = massMark / hightMark ** 2;
 const BMIJohn = massJohn / (hightJohn * hightJohn);
