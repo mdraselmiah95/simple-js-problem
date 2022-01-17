22fa59fcccb1915f318743ce79f2374133dbf017
rasel <russelroy15@gmail.com>
2022-01-18 02:48:31 +0600

    javascript problems and solutions

--- javaScript_problems.js
+++ javaScript_problems.js
@@ -1,3 +1,18 @@
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphin win the trophy 🏅");
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log("Koalas win the trophy ✨");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("Both win the trophy 🧶");
+// }
+
+//Bonus
+
 const scoreDolphins = (96 + 108 + 89) / 3;
 const scoreKoalas = (88 + 91 + 110) / 3;
 
